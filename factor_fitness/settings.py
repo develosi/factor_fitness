7ffdce363d38ad8a8d889193ab72e3c79bdae219
develosi <96108993+develosi@users.noreply.github.com>
2022-09-18 23:17:43 +0100

    Update INSTALLED APPS section in settings

--- factor_fitness/settings.py
+++ factor_fitness/settings.py
@@ -38,6 +38,10 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'django.contrib.sites',
+    'allauth',
+    'allauth.account',
+    'allauth.socialaccount',
 ]
 
 MIDDLEWARE = [
