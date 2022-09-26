71e2153770d355732c0cdcbd450a41f009e00bd7
develosi <96108993+develosi@users.noreply.github.com>
2022-09-26 22:44:13 +0100

    Fix python files to pep8 standards

--- factor_fitness/settings.py
+++ factor_fitness/settings.py
@@ -69,7 +69,7 @@ TEMPLATES = [
         'OPTIONS': {
             'context_processors': [
                 'django.template.context_processors.debug',
-                'django.template.context_processors.request', # REQUIRED BY ALLAUTH 
+                'django.template.context_processors.request',  # REQUIRED BY ALLAUTH 
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
             ],
