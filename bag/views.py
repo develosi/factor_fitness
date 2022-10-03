2cd613349470e8e6edac71fe1dd5b0ced24c98b1
develosi <96108993+develosi@users.noreply.github.com>
2022-10-03 14:52:54 +0100

    Add temaplates directory & initial code for views

--- bag/views.py
+++ bag/views.py
@@ -1,3 +1,12 @@
-from django.shortcuts import render
+from django.shortcuts import (
+    render, redirect, reverse, HttpResponse, get_object_or_404
+)
+from django.contrib import messages
 
-# Create your views here.
+from products.models import Product
+
+
+def view_bag(request):
+    """ A view that renders the bag contents page """
+
+    return render(request, 'bag/bag.html')
